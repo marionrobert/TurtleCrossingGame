3b829327d9d36d48b2995d09c76ba1dfd5a6de7a
Marion <mrobert148@gmail.com>
2023-02-27 15:36:33 +0100

    create random cars, move them and detect collision with them

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super(CarManager, self).__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.setheading(180)
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.goto(280, random.randint(-250, 250))
+        self.move_distance = STARTING_MOVE_DISTANCE
+
+    def move(self):
+        self.forward(self.move_distance)
+
+    def increase_move_distance(self):
+        self.move_distance += MOVE_INCREMENT
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,7 +11,26 @@ screen.tracer(0)
 
 player = Player()
 
+screen.listen()
+screen.onkeypress(fun=player.move, key="Up")
+
 game_is_on = True
+loops = 0
+cars = []
 while game_is_on:
+    loops += 1
+    if loops % 6 == 0:
+        new_car = CarManager()
+        cars.append(new_car)
+    for car in cars:
+        car.move()
+    # detect collision with a car
+    for car in cars:
+        if player.distance(car) < 25:
+            game_is_on = False
+            print("You loose")
     time.sleep(0.1)
     screen.update()
+
+
+screen.exitonclick()
